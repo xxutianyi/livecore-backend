d43ae22bd85410ee42ab54368b24ab8bddb821ce
徐添翼 XuTianyi <xutianyi12@outlook.com>
2026-04-21 10:04:36 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:clear-online')->everyMinute();
 Schedule::command('app:clear-expired-event')->everyFifteenMinutes();
+
+Schedule::command('app:collect-room-stats')->everyTenSeconds();
+Schedule::command('app:collect-room-stats')->everyTenSeconds();
